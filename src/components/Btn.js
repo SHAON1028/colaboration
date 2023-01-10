112229d146761415cb2d5614ec7ffc0e83313f20
SHAON1028 <shaonkhairul@gmail.com>
2023-01-10 21:38:47 +0600

    first commit

---
+++ src/components/Btn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Btn = () => {
+    return (
+        <div>
+            <button className="btn btn-active btn-primary">Button</button>
+        </div>
+    );
+};
+
+export default Btn;
