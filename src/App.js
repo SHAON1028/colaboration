b42b44520f1fe5eca7c8ec3ae50c908cfe25cf4e
SHAON1028 <shaonkhairul@gmail.com>
2023-01-11 14:53:02 +0600

    linux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Btn from './components/Btn';
+import logo from "./logo.svg";
+import "./App.css";
+import Btn from "./components/Btn";
 
 function App() {
   return (
     <div className="App">
-     <Btn></Btn>
+      <Btn></Btn>
+      <button className="btn btn-active btn-primary">Button from linux</button>
     </div>
   );
 }
